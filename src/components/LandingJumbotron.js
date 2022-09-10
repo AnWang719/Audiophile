30497b20e4078988e7813e759ab1ca8a98596d52
An Wang <anwang719@gmail.com>
2022-09-10 18:08:02 +1200

    styling jumbotron

--- src/components/LandingJumbotron.js
+++ src/components/LandingJumbotron.js
@@ -2,28 +2,45 @@ import classes from "./Jumbotron.module.css";
 
 function LandingJumbotron() {
   return (
-    <div className="jumbotron bgblack ">
-      {/* <div>
-        <span>NEW PRODUCT</span>
-        <h1 className="display-4">Hello, world!</h1>
-        <p className="lead">
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
-        </p>
+    <div className={classes.jumbotron}>
+      <span>NEW PRODUCT</span>
+      <h1 className="display-4">Hello, world!</h1>
+      <p className="lead">
+        This is a simple hero unit, a simple jumbotron-style component for
+        calling extra attention to featured content or information.
+      </p>
 
-        <p>
-          It uses utility classes for typography and spacing to space content
-          out within the larger container.
-        </p>
-        <p class="lead">
-          <a class="btn btn-primary btn-lg" href="#" role="button">
-            Learn more
-          </a>
-        </p>
-      </div> */}
-      <div className={`container ${classes.JumbotronContainer}`}>
+      <p>
+        It uses utility classes for typography and spacing to space content out
+        within the larger container.
+      </p>
+      <p className="lead">
+        <a className="btn btn-primary btn-lg" href="#" role="button">
+          Learn more
+        </a>
+      </p>
+
+      {/* <div className={`container ${classes.JumbotronContainer}`}>
         <img src={require("../assets/image-hero.jpg")} alt="new product" />
-      </div>
+        <div className="jumbotron">
+          <span>NEW PRODUCT</span>
+          <h1 className="display-4">Hello, world!</h1>
+          <p className="lead">
+            This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+
+          <p>
+            It uses utility classes for typography and spacing to space content
+            out within the larger container.
+          </p>
+          <p className="lead">
+            <a className="btn btn-primary btn-lg" href="#" role="button">
+              Learn more
+            </a>
+          </p>
+        </div>
+      </div> */}
     </div>
   );
 }

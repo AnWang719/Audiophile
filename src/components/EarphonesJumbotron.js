a60621515cf562eebdba39982158af773439ee4c
An Wang <anwang719@gmail.com>
2022-09-14 23:44:52 +1200

    earphone jumbotron styling

--- src/components/EarphonesJumbotron.js
+++ src/components/EarphonesJumbotron.js
@@ -7,12 +7,12 @@ function EarphonesJumbotron() {
   return (
     <Container className={classes.EarphonesJumbotron}>
       <Row>
-        <Col xs={12} md={6}>
+        <Col xs={12} md={6} className={classes.col}>
           <div className={classes.EarphonesImg}>
             <Image src={earphones} fluid />
           </div>
         </Col>
-        <Col xs={12} md={6}>
+        <Col xs={12} md={6} className={classes.col}>
           <div className={classes.EarphonesText}>
             <h1>YX1 EARPHONES</h1>
             <ProductButton className={classes.btn}>SEE PRODUCT</ProductButton>
